d1daaa5a1e71d39399d45c326a8d1fd4c01be07f
daniel florido <70490376+DanielFlorido@users.noreply.github.com>
2023-03-16 21:43:23 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Taller3/funciones.h
@@ -0,0 +1,8 @@
+#ifndef FUNCIONES_H
+#include <string.h>
+
+template<class _T>
+char* leerArchivo(const string& nombreArchivo);
+
+#endif // !FUNCIONES_H
+#pragma once
---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include <fstream>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+using namespace std;
+
+template <class _T> void leerArchivo(const string &nombreArchivo);
+
+int main(int argc, char **argv) {
+  if (argc > 3) {
+    cerr << "Usage: " << argv[0] << "input byte/word" << endl;
+    return -1;
+  }
+  string nombrearchivo = argv[1];
+  string tipo = argv[2];
+  if (tipo == "byte") {
+    leerArchivo<unsigned char>(nombrearchivo);
+  } else if (tipo == "word") {
+    leerArchivo<unsigned short>(nombrearchivo);
+  }
+  return (0);
+}
+
+template <class _T> void leerArchivo(const string &nombreArchivo) {
+  ifstream entrada(nombreArchivo.c_str(), ios::binary | ios::ate);
+  unsigned long long size = entrada.tellg();
+  entrada.seekg(0, ios::beg);
+
+  vector<_T> data(size / sizeof(_T));
+  char *buffer = reinterpret_cast<char *>(data.data());
+  entrada.read(buffer, data.size() * sizeof(_T));
+  entrada.close();
+
+  // Histograma
+  vector<double> H((1 << ((sizeof(_T)) << 3)), 0);
+  for (unsigned long long i = 0; i < data.size(); ++i) {
+    H[data[i]] += double(1) / (double(data.size()));
+  }
+  for (unsigned long long i = 0; i < H.size(); ++i) {
+    if (H[i] != 0) {
+      cout << i << " " << H[i] << endl;
+    }
+  }
+}
